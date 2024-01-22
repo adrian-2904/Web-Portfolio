64acebd217b593af05555904315bd951b3deabdd
adrian-2904 <156438279+adrian-2904@users.noreply.github.com>
2024-01-22 23:08:05 +0700

    Contact Update

--- main.js
+++ main.js
@@ -10,3 +10,33 @@ menuIcon.onclick = () => {
 function project(){
     alert("there are no projects in here")
 }
+
+const form = document.querySelector('form')
+const fullName = document.getElementById('name')
+const email = document.getElementById('email')
+const phoneNumber = document.getElementById('phone')
+const subject = document.getElementById('subject')
+const massage = document.getElementById('massage')
+
+function sendEmail() {
+    const bodyMassage = `Full name: ${fullName.value}<br> Email: ${email.value}
+    <br> Phone Number: ${phoneNumber.value}<br> Massage: ${massage.value}`;
+
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "esgaming0407@gmail.com",
+        Password : "DBD86D6E1C3C14F896A247FD84F01DB53E1A",
+        To : 'esgaming0407@gmail.com',
+        From : "esgaming0407@gmail.com",
+        Subject : subject.value,
+        Body : bodyMassage
+    }).then(
+        message => alert("Success")
+    );
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    sendEmail();
+});
